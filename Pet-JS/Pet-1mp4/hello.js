1a318e496f9333975e294116612ed5c100d0cf8c
Predrag05 <sim.pedja@gmail.com>
2023-01-19 06:05:25 +0100

    begining of Java Script code

---
+++ Pet-JS/Pet-1mp4/hello.js
@@ -0,0 +1,27 @@
+// Prvo doci u folder u kome je bas ovaj fajl hello.js
+// Kada se dodje u folder onda kucati komandu node (pa ime fajla) pa enter
+// node hello.js
+// Ispisace u nodeu samo ako ono sto se trazi u console logu
+// Pod navodnicima je string odnosno slova a bez navodnika pisemo brojeve
+// Moze tacka-zarez na kraju reda ili ne mora
+// Svejedno je da li je singl ili dupli navodnici za string
+
+
+console.log('Hello world!')
+
+console.log(5*31)
+console.log(5/31)
+console.log(5-31)
+console.log(5+31)
+
+// Promenljive
+var x = 4; // Deklaracija promenljive x
+x = 4; // Definicija promenljive x
+
+let y = 5; // Deklaracija i definicija promenljive y
+const z = 6; 
+
+console.log(x,y,z)
+
+x = 54;
+console.log(x,y,z)
