9d3a4800ef90868633af3aa73f980374003a2fdf
Predrag05 <sim.pedja@gmail.com>
2023-01-19 17:08:10 +0100

    more of node

--- Pet-JS/Pet-2mp4/nodeulazakizlazak.js
+++ Pet-JS/Pet-2mp4/nodeulazakizlazak.js
@@ -4,3 +4,12 @@
 // IZLAZI SE TAKODJE NA CTRL + D!!!
 // IZLAZAK IZ NODE TAKODJE I KOMANDNOM .exit
 // sve je case sensitive
+
+// string je jaci tako da string sa brojem ostaje string samo se doda rec
+
+// u NODE-u dole u terminalu ako zelimo da pretvorimo string u broj onda kucamo npr. 5 + Number("423")
+// i onda ce NODE sabrati i bice 428
+
+//ovo dole u terminalu u Node-u sluzi da bi smo proverili kod koji gore posle zelimo da kucamo
+//i Node moze da nam kaze da li bi ta kombinacija bila dobra ili ne, greska ili undefined...
+
