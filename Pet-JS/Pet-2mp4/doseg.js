dcc326b9a9e67bfea36147259ebdcba9b8d5afeb
Predrag05 <sim.pedja@gmail.com>
2023-01-19 16:59:51 +0100

    scope doseg add dopuna + node enter and exit

--- Pet-JS/Pet-2mp4/doseg.js
+++ Pet-JS/Pet-2mp4/doseg.js
@@ -28,7 +28,6 @@ var x // Vidi se u celom fajlu
 
 // ovde gore konzol log daje undefined
 
-let y
 
 console.log(a)
 
@@ -36,3 +35,37 @@ console.log(a)
     var a = "hello"
     console.log(a)
 }
+
+var x = 5
+x = 3
+console.log(x)
+
+const y = 4
+//y = 2 // const ne moze da se menja i da nije ovo u komentaru to bi bila greska
+
+let z = 7
+z = 4
+console.log(z)
+
+// Dakle promenio se var x i let z, i svakako je u conzole logu nov rezultat
+
+{
+    var x2 = 5
+}
+console.log(x2) // var se vidi i van Scopea, odnosno bloka koda, u kome je napravljen
+
+
+// {
+//     const y2 = 3
+// }
+// console.log(y2) // const se ne vidi van scope-a u kom je napravljen
+//                 //ReferenceError: y2 is not defined
+
+// {
+//     let z2 = 3 // let se ne vidi van scope-a u kom je napravljen    
+// }
+// console.log(z2) //ReferenceError: z2 is not defined
+
+// MOZE DA SE UDJE U NODE!!!
+// DOLE U TERMINALU SE SAMO KUCA KOMANDA NODE PA ENTER!!!
+// IZLAZI SE IZ NODE-A CTRL+C DVA PUTA!!!
