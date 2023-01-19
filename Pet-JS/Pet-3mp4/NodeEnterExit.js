93e8e84512938c953c9061ac7a1d553b6d1774fd
Predrag05 <sim.pedja@gmail.com>
2023-01-19 17:15:13 +0100

    treci klip a ne drugi klip

---
+++ Pet-JS/Pet-3mp4/NodeEnterExit.js
@@ -0,0 +1,21 @@
+// MOZE DA SE UDJE U NODE!!!
+// DOLE U TERMINALU SE SAMO KUCA KOMANDA NODE PA ENTER!!!
+// IZLAZI SE IZ NODE-A CTRL+C DVA PUTA!!!
+// IZLAZI SE TAKODJE NA CTRL + D!!!
+// IZLAZAK IZ NODE TAKODJE I KOMANDNOM .exit
+// sve je case sensitive
+
+// string je jaci tako da string sa brojem ostaje string samo se doda rec
+
+// u NODE-u dole u terminalu ako zelimo da pretvorimo string u broj onda kucamo npr. 5 + Number("423")
+// i onda ce NODE sabrati i bice 428
+
+//ovo dole u terminalu u Node-u sluzi da bi smo proverili kod koji gore posle zelimo da kucamo
+//i Node moze da nam kaze da li bi ta kombinacija bila dobra ili ne, greska ili undefined...
+
+let x
+x = 5
+const p = 9
+
+console.log(x,p,x+p);
+console.log(x,p,x*p);
