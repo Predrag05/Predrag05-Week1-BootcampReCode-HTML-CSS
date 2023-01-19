f7c908e08c8aed4567d75c280a26202a6c0bfdea
Predrag05 <sim.pedja@gmail.com>
2023-01-19 17:20:03 +0100

    one more thing in third clip

--- Pet-JS/Pet-3mp4/NodeEnterExit.js
+++ Pet-JS/Pet-3mp4/NodeEnterExit.js
@@ -19,3 +19,7 @@ const p = 9
 
 console.log(x,p,x+p);
 console.log(x,p,x*p);
+
+// Kada se naredjuje komandom Number("423") - to se yove - Eksplicitna konverzija
+// Ako se forsira eksplicitnom konverzijom Number("6asdgsda87")
+// ovo gore u Nodeu daje NaN - not a number
